216cc902313bdcb87195651a7e61e51bc591a4c3
Matthew <mfleberfinger@gmail.com>
2022-04-14 17:49:06 -0400

    Jumping on the Clouds Revisited

---
+++ JumpingOnTheCloudsRevisited.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k) {
+        // This is an interesting for loop.
+        int e = 100;
+        for (int i = k; i % c.Length != 0; i += k) {
+            if (c[i % c.Length] == 0)
+                e--;
+            else
+                e -= 3;
+        }
+        // Subtract the energy for the last jump.
+        e -= c[0] == 0 ? 1 : 3;
+        return e;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = jumpingOnClouds(c, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
